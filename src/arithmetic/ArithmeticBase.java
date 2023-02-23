9b70b48634383ee5112c673fe911d33c9ab2a243
Ahmad Tarkmani <tarkmani@sheridancollege.ca>
2023-02-22 18:24:29 -0800

    ICE1

---
+++ src/arithmetic/ArithmeticBase.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arithmetic;
+
+import java.util.Scanner;
+
+/**
+ * This class takes String input plus,minus,divide and times from user and
+ * execute the arithmetic operation change the code to use enum instead String
+ * and mention the advantage of enum.
+ *
+ * @author sivagamasrinivasan
+ *
+ */
+public class ArithmeticBase {
+
+    public double x, y;
+
+    double calculate(double x, double y) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("choose arithmetic operation to Perform: ");
+        System.out.println("1- PLUS\n2- MINUS\n3- TIMES\n4- DIVIDE");
+        int c = sc.nextInt();
+        while (c < 1 || c > 4) {
+            System.out.println("choose arithmetic operation to Perform: ");
+            System.out.println("1- PLUS\n2- MINUS\n3- TIMES\n4- DIVIDE");
+            c = sc.nextInt();
+        }
+        operations o=operations.PLUS;
+        switch (c) {
+            case 1: {
+                o = operations.PLUS;
+                break;
+            }
+            case 2: {
+                o = operations.MINUS;
+                break;
+            }
+            case 3: {
+                o = operations.TIMES;
+                break;
+            }
+            case 4: {
+                o = operations.DIVIDE;
+                break;
+            }
+        }
+        switch (o) {
+            case PLUS:
+                return x + y;
+            case MINUS:
+                return x - y;
+            case TIMES:
+                return x * y;
+            case DIVIDE:
+                return x / y;
+            default:
+                throw new AssertionError("Unknown operations " + this);
+        }
+    }
+
+}
---
+++ src/arithmetic/operations.java
@@ -0,0 +1,13 @@
+
+package arithmetic;
+
+/**
+ *
+ * @author Ahmed
+ */
+public enum operations {
+    PLUS,
+    MINUS,
+    TIMES,
+    DIVIDE
+}
